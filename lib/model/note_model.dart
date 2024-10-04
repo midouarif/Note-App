6ac408e5ec1d659b42e793536737c506195358d0
midouarif <midouarif4@gmail.com>
2024-10-04 15:41:48 +0100

    Add NoteModel class

---
+++ lib/model/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String description;
+  final String date;
+  final int colour;
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.colour,
+  });
+}
