f0ba286e300b468bfa3e6b2fbfa4025cebb3f35b
midouarif <midouarif4@gmail.com>
2024-10-05 23:01:39 +0100

    create add note methode and provide it

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,8 +1,21 @@
+
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/model/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+   try {
+  var notesBox = Hive.box<NoteModel>('notes');
+  emit(AddNoteSuccess());
+   await notesBox.add(note);
+} catch (e) {
+  emit(AddNoteFailure(e.toString()));
+}
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/cubits/add_note/add_note_cubit.dart';
 import 'package:note_app/model/note_model.dart';
 import 'package:note_app/views/notes_view.dart';
 
@@ -14,13 +16,21 @@ class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+        
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
