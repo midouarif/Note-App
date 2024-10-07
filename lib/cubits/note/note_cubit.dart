943014d88b1cffaa018e916d18f5bd8102b4e7c8
midouarif <midouarif4@gmail.com>
2024-10-07 17:07:39 +0100

    Refactor NoteCubit to fetch all notes from Hive

--- lib/cubits/note/note_cubit.dart
+++ lib/cubits/note/note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:note_app/model/note_model.dart';
 
@@ -6,4 +7,14 @@ part 'note_state.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>('notes');
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NoteSuccess(notes));
+    } catch (e) {
+      emit(NoteFailure(e.toString()));
+      print(e.toString());
+    }
+  }
 }
